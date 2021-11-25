0fde13998aba37e068eb46caa5745692e1dc2ab8
Archangel767 <mjolnir365@gmail.com>
2021-11-24 18:15:14 -0700

    added necessary files and folders for netlify deployment

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <article>
+    <nuxt-content :document="article" />
+    <pre> {{ article }} </pre>
+
+  </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+
+    return { article }
+  }
+}
+</script>
+
+<style>
+
+</style>
